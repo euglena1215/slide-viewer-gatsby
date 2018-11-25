9eeae0b79f592a6b7f72e864c1c8243ef388e15a
Shintani Teppei <teppest1215@gmail.com>
2018-11-25 16:49:33 +0900

    いらないものを消した

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import IndexLayout from '../layouts'
 import Page from '../components/Page'
 import Container from '../components/Container'
+import { Link } from 'gatsby'
 
 export default (context: any) => {
   const { pageContext } = context
@@ -9,7 +10,15 @@ export default (context: any) => {
   return (
     <IndexLayout>
       <Page>
-        <Container>{JSON.stringify(allSlide)}</Container>
+        <Container>
+          {allSlide.map(slide => {
+            return (
+              <Link to={`/${slide.fileId}/`} key={slide.fileId}>
+                <img src={slide.imgUrl} alt="" width={200} />
+              </Link>
+            )
+          })}
+        </Container>
       </Page>
     </IndexLayout>
   )
